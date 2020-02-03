4f2fdc5a5854047fbb9dbe8857407bd79ec1c584
tqashish <55793231+tqashish@users.noreply.github.com>
2020-02-03 15:36:02 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PracComponent } from './prac/prac.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PracComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+
+
+
+  
+}
---
+++ src/app/prac/prac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prac',
+  templateUrl: './prac.component.html',
+  styleUrls: ['./prac.component.css']
+})
+export class PracComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
